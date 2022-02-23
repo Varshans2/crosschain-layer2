ba664164c9609aa8da32aca95636f68451822acd
Varshans2 <89915467+Varshans2@users.noreply.github.com>
2022-02-23 18:39:54 +0200

    Initialize fix/bootnode-discovery

---
+++ README.md
@@ -0,0 +1,19 @@
+
+![Banner](.github/banner.jpg)
+
+## Polygon Edge
+
+Polygon Edge is a modular and extensible framework for building Ethereum-compatible blockchain networks.
+
+This repository is the first implementation of Polygon Edge, written in Golang. Other implementations, written in other programming languages might be introduced in the future. If you would like to contribute to this or any future implementation, please reach out to [Polygon team](mailto:contact@polygon.technology).
+
+To find out more about Polygon, visit the [official website](https://polygon.technology/).
+
+WARNING: This is a work in progress so architectural changes may happen in the future. The code has not been audited yet, so please contact [Polygon team](mailto:contact@polygon.technology) if you would like to use it in production.
+
+## Documentation 📝
+
+If you'd like to learn more about the Polygon Edge, how it works and how you can use it for your project,
+please check out the **[Polygon Edge Documentation](https://sdk-docs.polygon.technology)**.
+
+There, you will find the most up to date information regarding potential features, as well as future roadmaps for the Edge.
---
+++ consensus/consensus.go
@@ -0,0 +1,70 @@
+package consensus
+
+import (
+	"context"
+	"github.com/0xPolygon/polygon-edge/protocol"
+	"log"
+
+	"github.com/0xPolygon/polygon-edge/blockchain"
+	"github.com/0xPolygon/polygon-edge/chain"
+	"github.com/0xPolygon/polygon-edge/network"
+	"github.com/0xPolygon/polygon-edge/secrets"
+	"github.com/0xPolygon/polygon-edge/state"
+	"github.com/0xPolygon/polygon-edge/txpool"
+	"github.com/0xPolygon/polygon-edge/types"
+	"github.com/hashicorp/go-hclog"
+	"google.golang.org/grpc"
+)
+
+// Consensus is the public interface for consensus mechanism
+// Each consensus mechanism must implement this interface in order to be valid
+type Consensus interface {
+	// VerifyHeader verifies the header is correct
+	VerifyHeader(parent, header *types.Header) error
+
+	// GetBlockCreator retrieves the block creator (or signer) given the block header
+	GetBlockCreator(header *types.Header) (types.Address, error)
+
+	// GetSyncProgression retrieves the current sync progression, if any
+	GetSyncProgression() *protocol.Progression
+
+	// Start starts the consensus
+	Start() error
+
+	// Close closes the connection
+	Close() error
+}
+
+// Config is the configuration for the consensus
+type Config struct {
+	// Logger to be used by the backend
+	Logger *log.Logger
+
+	// Params are the params of the chain and the consensus
+	Params *chain.Params
+
+	// Config defines specific configuration parameters for the backend
+	Config map[string]interface{}
+
+	// Path is the directory path for the consensus protocol tos tore information
+	Path string
+}
+
+type ConsensusParams struct {
+	Context        context.Context
+	Seal           bool
+	Config         *Config
+	Txpool         *txpool.TxPool
+	Network        *network.Server
+	Blockchain     *blockchain.Blockchain
+	Executor       *state.Executor
+	Grpc           *grpc.Server
+	Logger         hclog.Logger
+	Metrics        *Metrics
+	SecretsManager secrets.SecretsManager
+}
+
+// Factory is the factory function to create a discovery backend
+type Factory func(
+	*ConsensusParams,
+) (Consensus, error)
